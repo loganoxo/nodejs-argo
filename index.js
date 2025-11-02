649a6a9c3b1df43e5b8f7a07df7b77ecda046a8d
logan <40782521+loganoxo@users.noreply.github.com>
2025-11-03 06:14:21 +0800

    隐藏私密信息

--- index.js
+++ index.js
@@ -11,18 +11,35 @@ const UPLOAD_URL = process.env.UPLOAD_URL || '';      // 节点或订阅自动�
 const PROJECT_URL = process.env.PROJECT_URL || '';    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活，true开启,需同时填写PROJECT_URL变量
 const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录
-const SUB_PATH = process.env.SUB_PATH || '';       // 订阅路径
+let SUB_PATH = process.env.A || '';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
 const UUID = process.env.UUID || '77cece1a-186c-44f3-999e-39fde93c0098'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+let ARGO_DOMAIN = process.env.B || '';          // 固定隧道域名,留空即启用临时隧道
+let ARGO_AUTH = process.env.C || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'railway-';                        // 节点名称
+
+//解密
+const crypto = require("crypto")
+const key = crypto.createHash("sha256").update("bbMXwj24nhu73o4A").digest() // 生成 32 字节密钥
+const iv = Buffer.from("GddgwiSJj4hHsw72") // 固定 16 字节 IV（也可自定义）
+// 解密
+function decrypt(encrypted) {
+    const encryptedBuf = Buffer.from(encrypted, "base64")
+    const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv)
+    let decrypted = decipher.update(encryptedBuf)
+    decrypted = Buffer.concat([decrypted, decipher.final()])
+    return decrypted.toString("utf8")
+}
+SUB_PATH=decrypt(SUB_PATH)
+ARGO_DOMAIN=decrypt(ARGO_DOMAIN)
+ARGO_AUTH=decrypt(ARGO_AUTH)
+
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
   fs.mkdirSync(FILE_PATH);
